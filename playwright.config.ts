56cbc4129ff12b8c2297c8167e5a51647e8c140d
Max Milton <max@happyhackers.xyz>
2024-08-04 20:33:18 +0900

    test: Improve test tooling

---
+++ playwright.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testMatch: 'test/e2e/**/*.spec.ts',
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
+  use: {
+    acceptDownloads: false,
+    contextOptions: { strictSelectors: true },
+    locale: 'en-US',
+    offline: true, // the extension must work 100% offline
+    timezoneId: 'UTC',
+    trace: 'on-first-retry',
+  },
+});
