321798cdff3757bbbf8aaf1178691e38223afa25
Max Milton <max@happyhackers.xyz>
2024-08-04 20:41:52 +0900

    chore: Rework and simplify build

--- build.ts
+++ build.ts
@@ -1,5 +1,12 @@
 import { createManifest } from './manifest.config';
-import rules from './rules';
+import { createRules } from './rules.config';
 
+console.time('prebuild');
+await Bun.$`rm -rf dist`;
+await Bun.$`cp -r static dist`;
+console.timeEnd('prebuild');
+
+console.time('build');
 await Bun.write('dist/manifest.json', JSON.stringify(createManifest()));
-await Bun.write('dist/rules.json', JSON.stringify(rules));
+await Bun.write('dist/rules.json', JSON.stringify(createRules()));
+console.timeEnd('build');
