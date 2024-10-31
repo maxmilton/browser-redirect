9259c6b148170cf77c959426a094b547692849fa
Max Milton <max@happyhackers.xyz>
2024-10-31 12:32:24 +0900

    chore: Tweak playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  testDir: 'test/e2e',
   testMatch: 'test/e2e/**/*.spec.ts',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
