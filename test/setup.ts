52646b6d3e34cab2860afd2190b9839523938e04
Max Milton <max@2okki.com>
2025-11-06 15:27:39 +0900

    test: Clean up unused setup code

--- test/setup.ts
+++ test/setup.ts
@@ -1,12 +1,1 @@
 import "@maxmilton/test-utils/extend";
-
-const noop = () => {};
-
-function setupMocks(): void {
-  // @ts-expect-error - noop stub
-  globalThis.performance.mark = noop;
-  // @ts-expect-error - noop stub
-  globalThis.performance.measure = noop;
-}
-
-setupMocks();
