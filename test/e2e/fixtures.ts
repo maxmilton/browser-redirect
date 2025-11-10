a5490fcc57773bc48380623ea30367c5a64ce5cc
Max Milton <max@2okki.com>
2025-11-10 14:00:29 +0900

    test: Clean up

--- test/e2e/fixtures.ts
+++ test/e2e/fixtures.ts
@@ -6,15 +6,12 @@ import path from "node:path";
 import { type BrowserContext, test as base, chromium } from "@playwright/test";
 
 export const test = base.extend<{ context: BrowserContext }>({
-  // biome-ignore lint/correctness/noEmptyPattern: empty initial context
+  // biome-ignore lint/correctness/noEmptyPattern: playwright setup
   async context({}, use) {
-    const extensionPath = path.join(import.meta.dirname, "../../dist");
+    const dist = path.join(import.meta.dirname, "../../dist");
     const context = await chromium.launchPersistentContext("", {
-      channel: "chromium", // enables headless mode with extensions
-      args: [`--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`],
-      acceptDownloads: false,
-      strictSelectors: true,
-      offline: true, // the extension must work 100% offline
+      channel: "chromium",
+      args: [`--disable-extensions-except=${dist}`, `--load-extension=${dist}`],
     });
     await use(context);
     await context.close();
